e6a39320da723c7a4e961f9009dfbd2f97d4c3e2
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-04-27 13:51:48 +0530

    Phase -2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react"
-import Button from "./components/Button"
 import ButtonGradient from "./assets/svg/ButtonGradient"
+import Header from "./components/Header"
 
-  const App=()=>{
+const App=()=>{
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-     Hello World</h1>
       <div className="pt-[4.75rem] lg:pt-[6.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-        Something
-        </Button>
-
+        {/* <Button className="mt-10" href="#login"> */}
+        {/* Something
+        </Button> */}
+        <Header/>
       </div>
       <ButtonGradient />
     </>
